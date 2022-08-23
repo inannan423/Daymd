78c935c29db2f122156e84b554633eed0094da3b
inannan423 <605031236@qq.com>
2022-08-23 21:58:49 +0800

    indexNew

--- components/CoolHead.jsx
+++ components/CoolHead.jsx
@@ -9,27 +9,75 @@ const PageData = {
 };
 
 function Zhanwei() {
-  return <div className="w-screen h-52"></div>;
+  return <div className="w-auto h-32"></div>;
 }
 
 function Title() {
   return (
-    <div className="w-screen h-10 flex justify-center items-center">
-      <span className="w-30 colorful  text-center font-black text-7xl">
+    <div className="md:w-auto h-30 overflow-hidden flex justify-center items-center">
+      <span className="colorful pl-5 pr-5 text-center font-black text-7xl">
         {PageData.headTitle} <br />
         {PageData.headTitle1} <br />
         <p className="text-3xl">{PageData.headTitleChinese}</p>
       </span>
-      <div className="circle1"></div>
-      <div className="circle2"></div>
+      <div className="overflow-hidden">
+        <div className="circle1"></div>
+        {/* <div class="csize absolute bg-blue-600 leftmax top-1 opacity-30 rounded-full blur-3xl -z-10"></div>
+        <div class="csize absolute bg-purple-500 rightmax top-1 opacity-30 rounded-full blur-3xl -z-10"></div> */}
+      </div>
     </div>
   );
 }
 
 function BigMax() {
   return (
-    <div className="bigMax mt-36">
+    <div className="bigMax h-auto mt-36 mb-28">
       <img src="https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/ProductWe1253.png" />
+    </div>
+  );
+}
+
+function Coding() {
+  return (
+    <div class="2xl:hidden">
+      <div className="w-screen mb-20  h-10 flex justify-center items-center">
+        <span className="mb-20 mt-16  w-30 text-center font-black largetext">
+          核心理念
+        </span>
+      </div>
+      <div class="md:flex gap-20 mb-20 sm:block flex justify-center items-center overflow-hidden">
+        <div class="h-max w-max  mockup-code">
+          <pre data-prefix="0">
+            <code>title: "Daymd.", </code>
+          </pre>
+          <pre data-prefix="1" class="text-warning">
+            <code>heroMode: 0, </code>
+          </pre>
+          <pre data-prefix="2" class="text-success">
+            <code>isNavLogo: 1, </code>
+          </pre>
+          <pre data-prefix="3" class="text-warning">
+            <code>heroButton: "Get Start", </code>
+          </pre>
+          <pre data-prefix="4" class="text-success">
+            <code>heroButtonLink: "/posts/change-me", </code>
+          </pre>
+          <pre data-prefix="5" class="text-warning">
+            <code>favicon: "https://jetzihan-img/daymd_logo1.svg", </code>
+          </pre>
+        </div>
+        <div class="card h-52 w-96  bg-primary text-primary-content">
+          <div class="card-body">
+            <h2 class="card-title">“设置式” 构建</h2>
+            <p>
+              就像手机设置一样，对不同的选项进行设置，就能达到你想要的结果。
+            </p>
+            <div class="card-actions justify-end">
+              <button class="btn">查看 API</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
@@ -54,6 +102,7 @@ export function CoolHead() {
       <Title />
       <BigMax />
       <Feter />
+      <Coding />
       <StartButton />
     </div>
   );
--- components/Feter.jsx
+++ components/Feter.jsx
@@ -8,26 +8,29 @@ const FeatureList = [
   {
     title: "零基础上手",
     file: "https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/banner1.png",
+    desc: "全面封装API,只需要填写预留的设置选项,就可以快速搭建出个人网站",
   },
   {
     title: "高度自定义",
     file: "https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/banner2.png",
+    desc: "Next.js应用，可用React快速自定义样式，增加新功能，预留多种主题",
   },
   {
-    title: "迅速部署上线",
+    title: "低代码迅速部署上线",
     file: "https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/banner3.png",
+    desc: "不需要任何环境准备，不需要写代码，快速自动化部署上线",
   },
 ];
 
-function Feature({ file, title, description }) {
+function Feature({ file, title, desc }) {
   return (
     <div className="flex flex-col hover:opacity-70 transition duration-500 ease-in-out">
-      <div className="text--center">
+      <div className="text-center">
         <img src={file} alt="banner" />
       </div>
-      <div className="text-center">
-        <h3 className="text-2xl">{title}</h3>
-        <p>{description}</p>
+      <div className="text-center pl-25 pr-25">
+        <h3 className="text-lg">{title}</h3>
+        <p className="text-center  text-sm">{desc}</p>
       </div>
     </div>
   );
@@ -37,12 +40,12 @@ export function Feter() {
   return (
     <div>
       <div className="w-screen h-10 flex justify-center items-center">
-        <span className="mb-20 w-30 text-center font-black text-5xl">
-          Daymd's Feature
+        <span className="mb-20 w-30 text-center font-black largetext">
+          Daymd's Features
         </span>
       </div>
-      <div className="w-screen mb-10  h-30 flex space-x-4 justify-center items-center">
-        <div className="flex gap-x55 ">
+      <div className="gap-x-2.5  w-screen mb-10  h-30 ">
+        <div className="flex flex-row flex-nowrap auto-cols-max">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
