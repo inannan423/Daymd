887d7e5f0e7fcc4df1a168d50828f1cb19c8e569
HanaHoshikawa <hanax092@outlook.com>
2022-10-22 09:25:50 +0000

    Update 34 files

--- navData/friend.ts
+++ navData/friend.ts
@@ -14,6 +14,11 @@ export const Friends: Friend[] = [
     website: "https://jetzihan.netlify.app/",
     avatar: "https://avatars.githubusercontent.com/u/83146544?v=4",
   },
+  {
+    title: "Rasp505",
+    description: "拉斯普的月台",
+    website: "https://blog.rasp505.top/",
+  },
 ];
 
 export function sortFriend() {
